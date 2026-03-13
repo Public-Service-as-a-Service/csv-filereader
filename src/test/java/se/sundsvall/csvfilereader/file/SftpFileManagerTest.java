ee64154713c2c8643b7143cfe9864de20bbb680a
Joakim Ring <joakim.ring@icloud.com>
2026-03-13 10:21:07 +0100

    sonar fix

--- src/test/java/se/sundsvall/csvfilereader/file/SftpFileManagerTest.java
+++ src/test/java/se/sundsvall/csvfilereader/file/SftpFileManagerTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class SftpFileManagerTest {
+class SftpFileManagerTest {
 
 	@Mock
 	private SftpProperties sftpProperties;
